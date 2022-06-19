a9cdae627ef8b3f3e9691852463c1a4c31cb6a82
Eithea <oguraff94@gmail.com>
2022-06-20 00:16:36 +0900

    go pj2

---
+++ Learn-Go/main.go
@@ -0,0 +1,90 @@
+//ch26/ex26.4/ex26.4.go
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type LineInfo struct {
+	lineNo int
+	line   string
+}
+
+type FindInfo struct {
+	filename string
+	lines    []LineInfo
+}
+
+func main() {
+	if len(os.Args) < 3 { //
+		fmt.Println("2개 이상의 실행인수가 필요합니다")
+		return
+	}
+
+	word := os.Args[1]
+	files := os.Args[2:]
+	findInfos := []FindInfo{}
+	for _, path := range files {
+		findInfos = append(findInfos, FindWordInAllFiles(word, path)...)
+	}
+
+	for _, findInfo := range findInfos {
+		fmt.Println(findInfo.filename)
+		for _, lineInfo := range findInfo.lines {
+			fmt.Println("\t", lineInfo.lineNo, "\t", lineInfo.line)
+		}
+		fmt.Println()
+	}
+}
+
+func FindWordInAllFiles(word, path string) []FindInfo {
+	findInfos := []FindInfo{}
+	filelist, err := filepath.Glob(path)
+	if err != nil {
+		fmt.Println("파일 경로가 잘못되었습니다. err:", err, "path:", path)
+		return findInfos
+	}
+
+	ch := make(chan FindInfo)
+	cnt := len(filelist)
+	recvCnt := 0
+
+	for _, filename := range filelist {
+		go FindWordInFile(word, filename, ch)
+	}
+
+	for findInfo := range ch {
+		findInfos = append(findInfos, findInfo)
+		recvCnt++
+		if recvCnt == cnt {
+			break
+		}
+	}
+	return findInfos
+}
+
+func FindWordInFile(word, filename string, ch chan FindInfo) {
+	findInfo := FindInfo{filename, []LineInfo{}}
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("파일을 찾을 수 없습니다. ", filename)
+		ch <- findInfo
+		return
+	}
+	defer file.Close()
+
+	lineNo := 1
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, word) {
+			findInfo.lines = append(findInfo.lines, LineInfo{lineNo, line})
+		}
+		lineNo++
+	}
+	ch <- findInfo
+}
